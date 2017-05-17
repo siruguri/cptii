4c562e64ab405d519c0471c7a36e0f70b81a24eb
Sameer Siruguri <siruguri@gmail.com>
2017-05-17 11:58:57 -0700

    Close #4

---
+++ db/seeds/make_goalgetter_counselor.rb
@@ -0,0 +1,14 @@
+# Goalgetter is everyone's counselor
+u = User.find_or_initialize_by email: 'info@goalgetterapp.org'
+u.password = ENV['ADMIN_PASSWORD']
+u.save
+
+p = u.profile
+p.contact_details = {first_name: 'GoalGetter', last_name: 'App'}
+p.save
+
+School.all.each do |s|
+  unless s.counselors.pluck(:id).include? u.id
+    s.counselors << u
+  end
+end
